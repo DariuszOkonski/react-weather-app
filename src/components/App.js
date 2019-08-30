291cae9e6fbf4cdd0801b141cfe0861d460c2baa
Dariusz Okonski <darek200180@gmail.com>
2019-08-30 22:16:19 +0100

    change App.js into stateless component

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HeaderComp from './HeaderComp';
 import MainComp from './MainComp';
 import FooterComp from './FooterComp';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <HeaderComp />
-        <MainComp />
-        <FooterComp />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="app">
+      <HeaderComp />
+      <MainComp />
+      <FooterComp />
+    </div>
+  );
 }
 
 export default App;

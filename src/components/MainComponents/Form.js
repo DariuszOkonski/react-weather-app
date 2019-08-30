a1e7490e3379971d6dffd50799aac28ee7b08b4d
Dariusz Okonski <darek200180@gmail.com>
2019-08-31 00:01:51 +0100

    write logic and change to stateless component

--- src/components/MainComponents/Form.js
+++ src/components/MainComponents/Form.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Form extends Component {
-  state = {
-    city: 'warszawa'
-  }
+const Form = (props) => {
+  return (
+    <form onSubmit={props.onFindCity} className="form">
+      <input
+        type="text"
+        className="form__input"
+        placeholder="Type city name..."
+        value={props.city}
+        onChange={props.onChange}
+      />
 
-  render() {
-    return (
-      <form className="form">
-        <input
-          type="text"
-          className="form__input"
-          placeholder="Type city name..."
-        />
+      <button className="form__btn">Find city</button>
 
-        <button className="form__btn">Find city</button>
-        <button className="form__btn">Clear</button>
-      </form>
-    );
-  }
+      <button
+        className="form__btn"
+        onClick={props.onClear}
+      >Clear</button>
+    </form>
+  );
 }
 
 export default Form;

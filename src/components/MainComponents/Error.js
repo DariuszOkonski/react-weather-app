5b29c5b163f9eef4ad24b3ec12c3b539534d3626
Dariusz Okonski <darek200180@gmail.com>
2019-08-31 00:02:55 +0100

    write Error component

---
+++ src/components/MainComponents/Error.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className="display">
+      <p className="display__error">
+        No Data Found
+      </p>
+    </div>
+  );
+}
+
+export default Error;
